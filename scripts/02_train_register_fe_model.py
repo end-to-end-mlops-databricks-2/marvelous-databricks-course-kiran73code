6740a1a7d257dd1ebc62d7308a48f8e635839ef4
Kiran CR <kirancr2000@gmail.com>
2025-02-18 23:59:29 +0530

    final commit for model registery

--- scripts/02_train_register_fe_model.py
+++ scripts/02_train_register_fe_model.py
@@ -1,6 +1,3 @@
-# Databricks notebook source
-# COMMAND ----------
-
 import mlflow
 from loguru import logger
 from pyspark.sql import SparkSession
@@ -17,43 +14,38 @@ spark = SparkSession.builder.getOrCreate()
 tags_dict = {"git_sha": "abcd12345", "branch": "week2"}
 tags = Tags(**tags_dict)
 
-
-# COMMAND ----------
-
 # Initialize model
 fe_model = FeatureLookUpModel(config=config, tags=tags, spark=spark)
-
-# COMMAND ----------
 
 # Create feature table
 fe_model.create_feature_table()
 
-# COMMAND ----------
+
 
 # Define is_weekend feature function
 fe_model.define_feature_function()
 
-# COMMAND ----------
+
 
 # Load data
 fe_model.load_data()
 
-# COMMAND ----------
+
 
 # Perform feature engineering
 fe_model.feature_engineering()
 
-# COMMAND ----------
+
 
 # Train the model
 fe_model.train()
 
-# COMMAND ----------
+
 
 # Train the model
 fe_model.register_model()
 
-# COMMAND ----------
+
 
 # Lets run prediction on the last production model
 # Load test set from Delta table
@@ -64,8 +56,6 @@ test_set = spark.table(f"{config.catalog_name}.{config.schema_name}.test_set").l
 # Drop feature lookup columns and target
 X_test = test_set.drop("payment_type_discount", config.target)
 
-
-# COMMAND ----------
 
 fe_model = FeatureLookUpModel(config=config, tags=tags, spark=spark)
 
