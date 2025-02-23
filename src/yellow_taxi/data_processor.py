8efb99a0a9efd941428b048216cc0b1031235091
Kiran CR <kirancr2000@gmail.com>
2025-02-23 10:01:45 +0530

    table changes for feature serving

--- src/yellow_taxi/data_processor.py
+++ src/yellow_taxi/data_processor.py
@@ -111,6 +111,10 @@ class DataProcessor:
         target = self.config.target
         relevant_columns = cat_features + num_features + [target]
         self.df = self.df[relevant_columns]
+        self.df.reset_index(drop=True, inplace=True)
+        # trip_id column is created for the purpose of tracking the records
+        self.df["trip_id"] = self.df.index + 1
+        self.df["trip_id"] = self.df["trip_id"].astype(str)
 
     def split_data(self, test_size=0.2, random_state=42):
         """Split the DataFrame (self.df) into training and test sets."""
