4158b3ed7a9a77c3e2d631c3c873f0b8a5b79c2e
Kiran CR <kirancr2000@gmail.com>
2025-03-05 23:40:57 +0530

    config changes for DAB's

--- src/yellow_taxi/config.py
+++ src/yellow_taxi/config.py
@@ -17,12 +17,23 @@ class ProjectConfig(BaseModel):
 
     @classmethod
     def from_yaml(cls, config_path: str, env: str = None):
-        """Load configuration from a YAML file."""
+        """Load configuration from a YAML file, adapting based on environment settings."""
         with open(config_path, "r") as f:
             config_dict = yaml.safe_load(f)
+
+        if env is not None:
+            config_dict["catalog_name"] = config_dict[env]["catalog_name"]
+            config_dict["schema_name"] = config_dict[env]["schema_name"]
+            config_dict["pipeline_id"] = config_dict[env]["pipeline_id"]
+        else:
+            config_dict["catalog_name"] = config_dict["catalog_name"]
+            config_dict["schema_name"] = config_dict["schema_name"]
+            config_dict["pipeline_id"] = config_dict["pipeline_id"]
+
         return cls(**config_dict)
 
 
 class Tags(BaseModel):
     git_sha: str
     branch: str
+    job_run_id: str
