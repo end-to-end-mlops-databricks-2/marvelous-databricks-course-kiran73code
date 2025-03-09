3b5f60bfea30cdf705930dfc8ef5a6e4dbfeab0a
Kiran CR <kirancr2000@gmail.com>
2025-03-09 21:02:31 +0530

    model monitoring: creating monitoring table

--- src/yellow_taxi/monitoring.py
+++ src/yellow_taxi/monitoring.py
@@ -12,6 +12,7 @@ def create_or_refresh_monitoring(config, spark, workspace):
     inf_table = spark.sql(
         f"SELECT * FROM {config.catalog_name}.{config.schema_name}.`yellow_taxi_fe_model_serving_payload`"
     )
+    
 
     request_schema = StructType(
         [
@@ -20,32 +21,16 @@ def create_or_refresh_monitoring(config, spark, workspace):
                 ArrayType(
                     StructType(
                         [
-                            StructField("LotFrontage", DoubleType(), True),
-                            StructField("LotArea", IntegerType(), True),
-                            StructField("OverallCond", IntegerType(), True),
-                            StructField("YearBuilt", IntegerType(), True),
-                            StructField("YearRemodAdd", IntegerType(), True),
-                            StructField("MasVnrArea", DoubleType(), True),
-                            StructField("TotalBsmtSF", IntegerType(), True),
-                            StructField("MSZoning", StringType(), True),
-                            StructField("Street", StringType(), True),
-                            StructField("Alley", StringType(), True),
-                            StructField("LotShape", StringType(), True),
-                            StructField("LandContour", StringType(), True),
-                            StructField("Neighborhood", StringType(), True),
-                            StructField("Condition1", StringType(), True),
-                            StructField("BldgType", StringType(), True),
-                            StructField("HouseStyle", StringType(), True),
-                            StructField("RoofStyle", StringType(), True),
-                            StructField("Exterior1st", StringType(), True),
-                            StructField("Exterior2nd", StringType(), True),
-                            StructField("MasVnrType", StringType(), True),
-                            StructField("Foundation", StringType(), True),
-                            StructField("Heating", StringType(), True),
-                            StructField("CentralAir", StringType(), True),
-                            StructField("SaleType", StringType(), True),
-                            StructField("SaleCondition", StringType(), True),
-                            StructField("Id", StringType(), True),
+                            StructField("fare_amount", DoubleType(), True),
+                            StructField("passenger_count", DoubleType(), True),
+                            StructField("trip_distance", DoubleType(), True),
+                            StructField("PULocationID", StringType(), True),
+                            StructField("transaction_month", IntegerType(), True),
+                            StructField("transaction_day", IntegerType(), True),
+                            StructField("transaction_hour", IntegerType(), True),
+                            StructField("transaction_year", IntegerType(), True),
+                            StructField("payment_type", IntegerType(), True),
+                           
                         ]
                     )
                 ),
@@ -72,62 +57,46 @@ def create_or_refresh_monitoring(config, spark, workspace):
     inf_table_parsed = inf_table_parsed.withColumn("parsed_response", F.from_json(F.col("response"), response_schema))
 
     df_exploded = inf_table_parsed.withColumn("record", F.explode(F.col("parsed_request.dataframe_records")))
-
+    
+    
     df_final = df_exploded.select(
         F.from_unixtime(F.col("timestamp_ms") / 1000).cast("timestamp").alias("timestamp"),
         "timestamp_ms",
         "databricks_request_id",
         "execution_time_ms",
-        F.col("record.Id").alias("Id"),
-        F.col("record.LotFrontage").alias("LotFrontage"),
-        F.col("record.LotArea").alias("LotArea"),
-        F.col("record.OverallCond").alias("OverallCond"),
-        F.col("record.YearBuilt").alias("YearBuilt"),
-        F.col("record.YearRemodAdd").alias("YearRemodAdd"),
-        F.col("record.MasVnrArea").alias("MasVnrArea"),
-        F.col("record.TotalBsmtSF").alias("TotalBsmtSF"),
-        F.col("record.MSZoning").alias("MSZoning"),
-        F.col("record.Street").alias("Street"),
-        F.col("record.Alley").alias("Alley"),
-        F.col("record.LotShape").alias("LotShape"),
-        F.col("record.LandContour").alias("LandContour"),
-        F.col("record.Neighborhood").alias("Neighborhood"),
-        F.col("record.Condition1").alias("Condition1"),
-        F.col("record.BldgType").alias("BldgType"),
-        F.col("record.HouseStyle").alias("HouseStyle"),
-        F.col("record.RoofStyle").alias("RoofStyle"),
-        F.col("record.Exterior1st").alias("Exterior1st"),
-        F.col("record.Exterior2nd").alias("Exterior2nd"),
-        F.col("record.MasVnrType").alias("MasVnrType"),
-        F.col("record.Foundation").alias("Foundation"),
-        F.col("record.Heating").alias("Heating"),
-        F.col("record.CentralAir").alias("CentralAir"),
-        F.col("record.SaleType").alias("SaleType"),
-        F.col("record.SaleCondition").alias("SaleCondition"),
+        F.col("record.fare_amount").alias("fare_amount"),
+        F.col("record.passenger_count").alias("passenger_count"),
+        F.col("record.trip_distance").alias("trip_distance"),
+        F.col("record.PULocationID").alias("PULocationID"),
+        F.col("record.transaction_month").alias("transaction_month"),
+        F.col("record.transaction_day").alias("transaction_day"),
+        F.col("record.transaction_hour").alias("transaction_hour"),
+        F.col("record.transaction_year").alias("transaction_year"),
+        F.col("record.payment_type").alias("payment_type"),
         F.col("parsed_response.predictions")[0].alias("prediction"),
-        F.lit("house-prices-model-fe").alias("model_name"),
+        F.lit("yellow-taxi-model-fe").alias("model_name"),
     )
 
     test_set = spark.table(f"{config.catalog_name}.{config.schema_name}.test_set")
     inference_set_skewed = spark.table(f"{config.catalog_name}.{config.schema_name}.inference_data_skewed")
 
     df_final_with_status = (
-        df_final.join(test_set.select("Id", "SalePrice"), on="Id", how="left")
-        .withColumnRenamed("SalePrice", "sale_price_test")
-        .join(inference_set_skewed.select("Id", "SalePrice"), on="Id", how="left")
-        .withColumnRenamed("SalePrice", "sale_price_inference")
-        .select("*", F.coalesce(F.col("sale_price_test"), F.col("sale_price_inference")).alias("sale_price"))
-        .drop("sale_price_test", "sale_price_inference")
-        .withColumn("sale_price", F.col("sale_price").cast("double"))
+        df_final.join(test_set.select("transaction_month","transaction_day","transaction_hour", "total_amount"), on=["transaction_month","transaction_day","transaction_hour"], how="left")
+        .withColumnRenamed("total_amount", "total_amount_test")
+        .join(inference_set_skewed.select("transaction_month","transaction_day","transaction_hour", "total_amount"), on=["transaction_month","transaction_day","transaction_hour"], how="left")
+        .withColumnRenamed("total_amount", "total_amount_inference")
+        .select("*", F.coalesce(F.col("total_amount_test"), F.col("total_amount_inference")).alias("total_amount"))
+        .drop("total_amount_test", "total_amount_inference")
+        .withColumn("total_amount", F.col("total_amount").cast("double"))
         .withColumn("prediction", F.col("prediction").cast("double"))
-        .dropna(subset=["sale_price", "prediction"])
+        .dropna(subset=["total_amount", "prediction"])
     )
 
-    house_features = spark.table(f"{config.catalog_name}.{config.schema_name}.house_features")
+    yellow_taxi_features = spark.table(f"{config.catalog_name}.{config.schema_name}.yellow_taxi_features")
 
-    df_final_with_features = df_final_with_status.join(house_features, on="Id", how="left")
+    df_final_with_features = df_final_with_status.join(yellow_taxi_features, on="payment_type", how="left")
 
-    df_final_with_features = df_final_with_features.withColumn("GrLivArea", F.col("GrLivArea").cast("double"))
+    df_final_with_features = df_final_with_features.withColumn("payment_type_discount", F.col("payment_type_discount").cast("double"))
 
     df_final_with_features.write.format("delta").mode("append").saveAsTable(
         f"{config.catalog_name}.{config.schema_name}.model_monitoring"
@@ -159,7 +128,7 @@ def create_monitoring_table(config, spark, workspace):
             timestamp_col="timestamp",
             granularities=["30 minutes"],
             model_id_col="model_name",
-            label_col="sale_price",
+            label_col="total_amount",
         ),
     )
 
